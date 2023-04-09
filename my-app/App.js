15423b2b9b3c685887dbda1d9cda379840df0d23
Anatolii-melashchenko <toramulo2012@gmail.com>
2023-04-09 19:11:34 +0300

    summary

--- my-app/App.js
+++ my-app/App.js
@@ -1,30 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { ImageBackground, StyleSheet, View } from "react-native";
 import RegistrationScreen from "./Screens/RegistrationScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <ImageBackground
-        style={styles.image}
-        source={require("./assets/images/photo-bg.jpg")}
-      >
-        <RegistrationScreen />
-        <StatusBar style="auto" />
-      </ImageBackground>
-    </View>
-  );
+  return <RegistrationScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-
-  image: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "flex-end",
-  },
-});
--- my-app/Screens/RegistrationScreen.jsx
+++ my-app/Screens/RegistrationScreen.jsx
@@ -1,4 +1,7 @@
+import { StatusBar } from "expo-status-bar";
+import { useState, useEffect } from "react";
 import {
+  ImageBackground,
   TextInput,
   View,
   StyleSheet,
@@ -7,57 +10,161 @@ import {
   Image,
   KeyboardAvoidingView,
   Platform,
+  Keyboard,
+  TouchableWithoutFeedback,
+  Dimensions,
 } from "react-native";
 
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+
+const initialState = {
+  login: "",
+  email: "",
+  password: "",
+};
+
+SplashScreen.preventAutoHideAsync();
+
 export default function RegistrationScreen() {
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [input, setInput] = useState(initialState);
+  const [fontsLoaded] = useFonts({
+    "ChakraPetch-Regular": require("../assets/fonts/ChakraPetch-Regular.ttf"),
+  });
+  const [dimensions, setDimensions] = useState(
+    Dimensions.get("window").width - 8 * 2
+  );
+
+  useEffect(() => {
+    onChange = () => {
+      const width = Dimensions.get("window").width - 8 * 2;
+      setDimensions(width);
+    };
+    Dimensions.addEventListener("change", onChange);
+    return () => {
+      Dimensions.removeEventListener("change", onChange);
+    };
+  }, []);
+
+  const keyboardHidden = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+  };
+
+  const onSubmit = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+    console.log(input);
+    setInput(initialState);
+  };
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-    >
-      <View style={styles.form}>
-        <View style={styles.imageContainer}>
-          <Image style={styles.image}></Image>
-        </View>
+    <TouchableWithoutFeedback onPress={keyboardHidden}>
+      <View style={styles.container} onLayout={onLayoutRootView}>
+        <ImageBackground
+          style={styles.image}
+          source={require("../assets/images/photo-bg.jpg")}
+        >
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : ""}
+          >
+            <View style={styles.form}>
+              <View style={styles.avatarContainer}>
+                <Image style={styles.avatar}></Image>
+              </View>
 
-        <View>
-          <Text style={styles.title}>Registration</Text>
-        </View>
-        <View>
-          <TextInput
-            style={styles.input}
-            placeholder="Login"
-            textAlign={"center"}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Email"
-            textAlign={"center"}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Password"
-            textAlign={"center"}
-            secureTextEntry={true}
-          />
-        </View>
+              <View>
+                <Text style={styles.title}>Registration</Text>
+              </View>
+              <View style={styles.inputWrapper}>
+                <TextInput
+                  style={{ ...styles.input, width: dimensions }}
+                  placeholder="Login"
+                  textAlign={"left"}
+                  onFocus={() => setIsShowKeyboard(true)}
+                  onChangeText={(value) =>
+                    setInput((prevState) => ({ ...prevState, login: value }))
+                  }
+                  value={input.login}
+                />
+                <TextInput
+                  style={{ ...styles.input, width: dimensions }}
+                  placeholder="Email"
+                  textAlign={"left"}
+                  onFocus={() => setIsShowKeyboard(true)}
+                  onChangeText={(value) =>
+                    setInput((prevState) => ({ ...prevState, email: value }))
+                  }
+                  value={input.email}
+                />
+                <TextInput
+                  style={{ ...styles.input, width: dimensions }}
+                  placeholder="Password"
+                  textAlign={"left"}
+                  secureTextEntry={true}
+                  onFocus={() => setIsShowKeyboard(true)}
+                  onChangeText={(value) =>
+                    setInput((prevState) => ({ ...prevState, password: value }))
+                  }
+                  value={input.password}
+                />
+              </View>
 
-        <View>
-          <TouchableOpacity activeOpacity={0.8} style={styles.btn}>
-            <Text style={styles.btnTitle}>Registration</Text>
-          </TouchableOpacity>
-        </View>
-        <View>
-          <Text style={styles.text}>Do you have account? Login</Text>
-        </View>
+              <View>
+                {!isShowKeyboard && (
+                  <TouchableOpacity
+                    activeOpacity={0.8}
+                    style={styles.btn}
+                    onPress={onSubmit}
+                  >
+                    <Text style={styles.btnTitle}>Registration</Text>
+                  </TouchableOpacity>
+                )}
+              </View>
+              <View>
+                {!isShowKeyboard && (
+                  <Text style={styles.text}>Do you have account? Login</Text>
+                )}
+              </View>
+            </View>
+          </KeyboardAvoidingView>
+          <StatusBar style="auto" />
+        </ImageBackground>
       </View>
-    </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+  },
   form: {
     backgroundColor: "#fff",
-    borderRadius: 25,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  inputWrapper: {
+    alignItems: "center",
   },
   input: {
     height: 50,
@@ -65,12 +172,13 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "#E8E8E8",
     borderRadius: 8,
-    marginHorizontal: 16,
+    // marginHorizontal: 16,
     marginBottom: 16,
     color: "#BDBDBD",
+    padding: 16,
   },
   title: {
-    fontFamily: "Roboto",
+    fontFamily: "ChakraPetch-Regular",
     fontStyle: "normal",
     fontWeight: 500,
     fontSize: 30,
@@ -90,7 +198,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   btnTitle: {
-    fontFamily: "Roboto",
+    fontFamily: "ChakraPetch-Regular",
     fontStyle: "normal",
     fontWeight: 400,
     fontSize: 16,
@@ -98,7 +206,7 @@ const styles = StyleSheet.create({
     color: "#FFFFFF",
   },
   text: {
-    fontFamily: "Roboto",
+    fontFamily: "ChakraPetch-Regular",
     fontStyle: "normal",
     fontWeight: 400,
     fontSize: 16,
@@ -108,14 +216,14 @@ const styles = StyleSheet.create({
     marginTop: 16,
     marginBottom: 45,
   },
-  image: {
+  avatar: {
     position: "absolute",
     width: 120,
     height: 120,
     backgroundColor: "#F6F6F6",
     borderRadius: 16,
   },
-  imageContainer: {
+  avatarContainer: {
     position: "relative",
     alignItems: "center",
     justifyContent: "center",
