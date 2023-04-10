8b66d07919184b3e8a0f7ffa10204964ac8ad266
Anatolii-melashchenko <toramulo2012@gmail.com>
2023-04-10 19:29:55 +0300

    summary

--- my-app/Screens/RegistrationScreen.jsx
+++ my-app/Screens/RegistrationScreen.jsx
@@ -13,11 +13,13 @@ import {
   Keyboard,
   TouchableWithoutFeedback,
   Dimensions,
+  Button,
 } from "react-native";
 
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { useCallback } from "react";
+import Icon from "react-native-vector-icons/FontAwesome5";
 
 const initialState = {
   login: "",
@@ -33,29 +35,45 @@ export default function RegistrationScreen() {
   const [fontsLoaded] = useFonts({
     "ChakraPetch-Regular": require("../assets/fonts/ChakraPetch-Regular.ttf"),
   });
-  const [dimensions, setDimensions] = useState(
-    Dimensions.get("window").width - 8 * 2
-  );
 
-  useEffect(() => {
-    onChange = () => {
-      const width = Dimensions.get("window").width - 8 * 2;
-      setDimensions(width);
-    };
-    Dimensions.addEventListener("change", onChange);
-    return () => {
-      Dimensions.removeEventListener("change", onChange);
-    };
-  }, []);
+  const [isFocused, setIsFocused] = useState({
+    login: false,
+    email: false,
+    password: false,
+  });
 
-  const keyboardHidden = () => {
+  const [hidePass, setHidePass] = useState(true);
+  // const [dimensions, setDimensions] = useState(
+  //   Dimensions.get("window").width - 8 * 2
+  // );
+
+  // useEffect(() => {
+  //   onChange = () => {
+  //     const width = Dimensions.get("window").width - 8 * 2;
+  //     setDimensions(width);
+  //   };
+  //   Dimensions.addEventListener("change", onChange);
+  //   return () => {
+  //     Dimensions.removeEventListener("change", onChange);
+  //   };
+  // }, []);
+
+  const handleInputFocus = (e) => {
+    setIsFocused({ [e]: true });
+    setIsShowKeyboard(true);
+  };
+
+  const handleInputBlur = (e) => {
+    setIsFocused({ [e]: false });
+    setIsShowKeyboard(false);
+  };
+
+  const onKeyboardHidden = () => {
     setIsShowKeyboard(false);
     Keyboard.dismiss();
   };
 
   const onSubmit = () => {
-    setIsShowKeyboard(false);
-    Keyboard.dismiss();
     console.log(input);
     setInput(initialState);
   };
@@ -71,7 +89,7 @@ export default function RegistrationScreen() {
   }
 
   return (
-    <TouchableWithoutFeedback onPress={keyboardHidden}>
+    <TouchableWithoutFeedback onPress={onKeyboardHidden}>
       <View style={styles.container} onLayout={onLayoutRootView}>
         <ImageBackground
           style={styles.image}
@@ -82,44 +100,95 @@ export default function RegistrationScreen() {
           >
             <View style={styles.form}>
               <View style={styles.avatarContainer}>
-                <Image style={styles.avatar}></Image>
+                <Image
+                  style={styles.avatar}
+                  source={require("../assets/images/Rectangle.jpg")}
+                ></Image>
+                <TouchableOpacity
+                  activeOpacity={0.8}
+                  style={styles.avatarBtn}
+                  onPress={onSubmit}
+                >
+                  <Image
+                    source={require("../assets/images/Union2.png")}
+                  ></Image>
+                </TouchableOpacity>
               </View>
 
               <View>
                 <Text style={styles.title}>Registration</Text>
               </View>
               <View style={styles.inputWrapper}>
-                <TextInput
-                  style={{ ...styles.input, width: dimensions }}
-                  placeholder="Login"
-                  textAlign={"left"}
-                  onFocus={() => setIsShowKeyboard(true)}
-                  onChangeText={(value) =>
-                    setInput((prevState) => ({ ...prevState, login: value }))
-                  }
-                  value={input.login}
-                />
-                <TextInput
-                  style={{ ...styles.input, width: dimensions }}
-                  placeholder="Email"
-                  textAlign={"left"}
-                  onFocus={() => setIsShowKeyboard(true)}
-                  onChangeText={(value) =>
-                    setInput((prevState) => ({ ...prevState, email: value }))
-                  }
-                  value={input.email}
-                />
-                <TextInput
-                  style={{ ...styles.input, width: dimensions }}
-                  placeholder="Password"
-                  textAlign={"left"}
-                  secureTextEntry={true}
-                  onFocus={() => setIsShowKeyboard(true)}
-                  onChangeText={(value) =>
-                    setInput((prevState) => ({ ...prevState, password: value }))
-                  }
-                  value={input.password}
-                />
+                <View>
+                  <TextInput
+                    style={
+                      isFocused.login
+                        ? [styles.input, { borderColor: "#FF6C00" }]
+                        : styles.input
+                    }
+                    placeholder="Login"
+                    textAlign={"left"}
+                    onFocus={() => handleInputFocus("login")}
+                    onBlur={() => handleInputBlur("login")}
+                    onChangeText={(value) =>
+                      setInput((prevState) => ({ ...prevState, login: value }))
+                    }
+                    value={input.login}
+                  />
+                </View>
+
+                <View>
+                  <TextInput
+                    style={
+                      isFocused.email
+                        ? [styles.input, { borderColor: "#FF6C00" }]
+                        : styles.input
+                    }
+                    placeholder="Email"
+                    textAlign={"left"}
+                    onFocus={() => handleInputFocus("email")}
+                    onBlur={() => handleInputBlur("email")}
+                    onChangeText={(value) =>
+                      setInput((prevState) => ({ ...prevState, email: value }))
+                    }
+                    value={input.email}
+                  />
+                </View>
+
+                <View style={styles.toShowPasswordWrapper}>
+                  <TextInput
+                    style={
+                      isFocused.password
+                        ? [styles.input, { borderColor: "#FF6C00" }]
+                        : styles.input
+                    }
+                    placeholder="Password"
+                    textAlign={"left"}
+                    secureTextEntry={hidePass ? true : false}
+                    onFocus={() => handleInputFocus("password")}
+                    onBlur={() => handleInputBlur("password")}
+                    onChangeText={(value) =>
+                      setInput((prevState) => ({
+                        ...prevState,
+                        password: value,
+                      }))
+                    }
+                    value={input.password}
+                  ></TextInput>
+                  <TouchableOpacity
+                    style={styles.toShowPassword}
+                    activeOpacity={0.8}
+                    onPress={() => setHidePass(!hidePass)}
+                  >
+                    <Text style={styles.toShowPasswordTitle}>
+                      {hidePass ? "to show" : "hide"}
+                    </Text>
+                    {/* <Icon
+                      name={hidePass ? "eye-slash" : "eye"}
+                      onPress={() => setHidePass(!hidePass)}
+                    /> */}
+                  </TouchableOpacity>
+                </View>
               </View>
 
               <View>
@@ -163,8 +232,39 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
   },
-  inputWrapper: {
+
+  avatarContainer: {
+    // position: "relative",
     alignItems: "center",
+    justifyContent: "center",
+
+    marginBottom: 100,
+  },
+  avatar: {
+    position: "absolute",
+    width: 120,
+    height: 120,
+    backgroundColor: "#F6F6F6",
+    borderRadius: 16,
+  },
+  avatarBtn: {
+    position: "absolute",
+    left: Platform.OS === "ios" ? 254 : 244,
+
+    top: 20,
+    height: 25,
+    width: 25,
+    borderWidth: 1,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#FFFFFF",
+    // borderColor: "#FF6C00",
+    borderColor: "#E8E8E8",
+  },
+  inputWrapper: {
+    // alignItems: "center",
+    marginBottom: 16,
   },
   input: {
     height: 50,
@@ -172,9 +272,14 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "#E8E8E8",
     borderRadius: 8,
-    // marginHorizontal: 16,
+    marginHorizontal: 16,
     marginBottom: 16,
-    color: "#BDBDBD",
+    color: "#212121",
+    fontFamily: "ChakraPetch-Regular",
+    fontStyle: "normal",
+    fontWeight: 400,
+    fontSize: 16,
+    lineHeight: 19,
     padding: 16,
   },
   title: {
@@ -189,13 +294,23 @@ const styles = StyleSheet.create({
     color: "#212121",
   },
   btn: {
-    backgroundColor: "#FF6C00",
     height: 51,
     marginHorizontal: 16,
+    borderWidth: 1,
     borderRadius: 100,
-    marginTop: 27,
+    marginTop: 11,
     alignItems: "center",
     justifyContent: "center",
+    ...Platform.select({
+      ios: {
+        backgroundColor: "transparent",
+        borderColor: "#FF6C00",
+      },
+      android: {
+        backgroundColor: "#FF6C00",
+        borderColor: "transparent",
+      },
+    }),
   },
   btnTitle: {
     fontFamily: "ChakraPetch-Regular",
@@ -203,7 +318,7 @@ const styles = StyleSheet.create({
     fontWeight: 400,
     fontSize: 16,
     lineHeight: 19,
-    color: "#FFFFFF",
+    color: Platform.OS === "ios" ? "#FF6C00" : "#FFFFFF",
   },
   text: {
     fontFamily: "ChakraPetch-Regular",
@@ -216,18 +331,21 @@ const styles = StyleSheet.create({
     marginTop: 16,
     marginBottom: 45,
   },
-  avatar: {
-    position: "absolute",
-    width: 120,
-    height: 120,
-    backgroundColor: "#F6F6F6",
-    borderRadius: 16,
-  },
-  avatarContainer: {
+  toShowPasswordWrapper: {
     position: "relative",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  toShowPassword: {
+    position: "absolute",
+    right: 32,
+    top: 16,
+  },
+  toShowPasswordTitle: {
+    fontFamily: "ChakraPetch-Regular",
+    fontStyle: "normal",
+    fontWeight: 400,
+    fontSize: 16,
+    lineHeight: 19,
 
-    marginBottom: 100,
+    color: "#1B4371",
   },
 });
